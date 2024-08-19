03e40170565573836ff9ddfe3f7169d0a9a40dbe
Emircan <emircandv787@gmail.com>
2024-08-19 23:59:21 +0300

    The Memcached configurations have been defined programcs

--- Cache.Memcached/Program.cs
+++ Cache.Memcached/Program.cs
@@ -1,8 +1,28 @@
+using Enyim.Caching;
+using Enyim.Caching.Configuration;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddEnyimMemcached(options =>
+{
+    options.Servers = new List<Server>()
+    {
+        new Server(){Address = builder.Configuration.GetValue<string>("Memcached:Address"),Port = builder.Configuration.GetValue<int>("Memcached:Port")}
+    };
+
+});
 
 var app = builder.Build();
 
@@ -12,6 +32,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseEnyimMemcached();
 
 app.UseHttpsRedirection();
 
